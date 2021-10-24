0a588edae2e4a36a799ae4f9d32ee1a603b05603
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-10-24 10:51:12 +0300

    add credentila test class

--- credential_test.py
+++ credential_test.py
@@ -10,24 +10,23 @@ class TestCredential(unittest.TestCase):
     unittest.TestCase: TestCase class that helps in creating credential test cases
   '''
 
+  def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_credential = Credential(
+            "James", "Muriuki", "0712345678", "james@ms.com")  # create contact object
 
-def setUp(self):
-    '''
-    Set up method to run before each test cases in credential
-    '''
-    self.new_credential = Credential(
-        "meshackkimutai", "1234567890", "0718908494")  # creating credential object
+  def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
 
+        self.assertEqual(self.new_credential.first_name, "James")
+        self.assertEqual(self.new_credential.last_name, "Muriuki")
+        self.assertEqual(self.new_credential.phone_number, "0712345678")
+        self.assertEqual(self.new_credential.email, "james@ms.com")
 
-def test_init(self):
-    '''
-    test_init test case to test if the object is initialized properly
-    '''
-
-    self.assertEqual(self.new_credential.credential_name,"meshackkimutai")
-    self.assertEqual(self.new_credential.password,"1234567890")
-    self.assertEqual(self.new_credential.number,"0718908494")
-
-
-if __name__ == '__main__':
+        
+  if __name__ == '__main__':
     unittest.main()
