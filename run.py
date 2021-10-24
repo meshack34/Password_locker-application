6fe46a9936470a486d1abc66c3762ad9091c5549
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-10-24 13:18:22 +0300

    create empy list for user

--- run.py
+++ run.py
@@ -3,7 +3,21 @@
 from credential import Credential
 from user import User
 
-def create_user(firstname, lastname, phonenumber, email):
+def mynew_user(firstname, lastname, phonenumber, email):
   ''' new user '''
   mynew_user = User(firstname, lastname, phonenumber, email)
   return mynew_user
+
+
+def save_mynew_user(user):
+  '''
+  Function to save user
+  '''
+  user.save_user()
+
+
+def delete_mynew_user(user):
+  '''
+  Function to delete a user
+  '''
+  user.delete_user()
---
+++ user.py
@@ -0,0 +1,9 @@
+import pyperclip
+
+
+class User:
+  '''
+  Class that creates new intances of users
+  '''
+
+  myuser_list = []  # empty user list
