b25c7736456c55613ee3bb410026cd5fa48eda70
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-10-24 10:28:17 +0300

    add the first test

--- credential_test.py
+++ credential_test.py
@@ -9,3 +9,25 @@ class TestCredential(unittest.TestCase):
   Args:
     unittest.TestCase: TestCase class that helps in creating credential test cases
   '''
+
+
+def setUp(self):
+    '''
+    Set up method to run before each test cases in credential
+    '''
+    self.new_credential = Credential(
+        "meshackkimutai", "1234567890", "0718908494")  # creating credential object
+
+
+def test_init(self):
+    '''
+    test_init test case to test if the object is initialized properly
+    '''
+
+    self.assertEqual(self.new_credential.credential_name,"meshackkimutai")
+    self.assertEqual(self.new_credential.password,"1234567890")
+    self.assertEqual(self.new_credential.number,"0718908494")
+
+
+if __name__ == '__main__':
+    unittest.main()
