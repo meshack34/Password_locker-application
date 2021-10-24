b52d17707512fc3f401e302bb8a3f0e55739dae8
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-10-24 12:58:33 +0300

    create user

--- run.py
+++ run.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3.6
+
+from credential import Credential
+
+#functions for user_account
+
+
+def create_user(fname, lname, phone, email):
+  '''
+  Function to create a new user
+  '''
+  new_user = User(fname, lname, phone, email)
+  return new_user
